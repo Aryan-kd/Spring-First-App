4260d7834d1e2af8677afa13f61861d9a61d5352
Aryan Kodinya <aryan.kodinya@gmail.com>
2024-08-18 18:42:04 +0530

    First Commit

---
+++ src/main/java/com/spring/FirstApp/FirstAppApplication.java
@@ -0,0 +1,13 @@
+package com.spring.FirstApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FirstAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FirstAppApplication.class, args);
+	}
+
+}
